d07ed3ae09714c8c93c3d79ab5dfcb3a4225a5fa
michallow <michallo_w@o2.pl>
2024-01-22 11:15:09 +0100

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
+
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+
+document.body.addEventListener("keypress", (e) => {
+  if (e.key === "Escape") basicLightbox.close();
+});
+
+for (let item of galleryItems) {
+  const galleryItem = document.createElement("div");
+  galleryItem.classList.add("gallery__item");
+
+  const galleryLink = document.createElement("a");
+  galleryLink.classList.add("gallery__link");
+  galleryLink.href = item.original;
+
+  const galleryImage = document.createElement("img");
+  galleryImage.classList.add("gallery__image");
+  galleryImage.src = item.preview;
+  galleryImage.dataset.source = item.original;
+  galleryImage.alt = item.description;
+
+  galleryLink.appendChild(galleryImage);
+  galleryItem.appendChild(galleryLink);
+  gallery.appendChild(galleryItem);
+}
+
+gallery.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  const lightbox = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+  `);
+
+  lightbox.show();
+});
+
+
 
 console.log(galleryItems);
