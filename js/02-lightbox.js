9181e30f2f43d1c49fc6a16b57e30d354f1bacbc
michallow <michallo_w@o2.pl>
2024-01-23 22:20:28 +0100

    Update

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,39 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
+
 // Change code below this line
+
+const gallery = document.querySelector(".gallery");
+
+for (let item of galleryItems) {
+  const galleryItem = document.createElement("div");
+  galleryItem.classList.add("gallery__item");
+
+  const galleryLink = document.createElement("a");
+  galleryLink.classList.add("gallery__link");
+  galleryLink.href = item.original;
+
+  const galleryImage = document.createElement("img");
+  galleryImage.classList.add("gallery__image");
+  galleryImage.src = item.preview;
+  galleryImage.alt = item.description;
+
+  galleryLink.appendChild(galleryImage);
+  galleryItem.appendChild(galleryLink);
+  gallery.appendChild(galleryItem);
+}
+
+gallery.addEventListener('click', (event) => {
+    event.preventDefault();
+  
+    const lightbox = new SimpleLightbox('.gallery a', {
+      captionsData: 'alt',
+      captionPosition: 'bottom',
+      captionClass: '',
+    });
+
+    lightbox.show();
+  
+    window.addEventListener('keyup', closeLightboxOnEscape);
+  });
 
 console.log(galleryItems);
